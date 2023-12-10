208e1a1226191ae17b331c370b2e72527567ea7b
Hervé Bourzeix <herve.bourzeix@teads.tv>
2023-12-11 00:05:20 +0100

    day 4 part 1

--- day04/main.ts
+++ day04/main.ts
@@ -4,5 +4,27 @@ import * as fs from 'fs'
 
 const input = fs.readFileSync('./input.sample.txt', 'utf-8')
 
-input.split('\n')
+console.info(input.split('\n').filter(line => line !== "").map((line) => {
+        const [, cardNumberStr, ownCardsStr, winningCardsStr] = /Card \s*(\d+):((?:\s*\d+\s*)+)\|((?:\s*\d+\s*)+)/.exec(line) || []
+        //console.info(cardNumberStr, ownCardsStr.length+1, winningCardsStr.length+1)
+        const result = {
+            cardNumber: Number(cardNumberStr),
+            ownCard: ownCardsStr.trim().replaceAll('  ', ' ').split(' ').map(value => Number(value)),
+            winningCards: winningCardsStr.trim().replaceAll('  ', ' ').split(' ').map(value => Number(value)),
+        }
+        //console.info(result.ownCard.length,result.winningCards.length)
+        return result
 
+    }
+).map(({ownCard, winningCards}) => ownCard.filter(x => winningCards.includes(x)))
+    .map((commonValues) => {
+
+        const points =  commonValues.reduce((points, currentValue, currentIndex) => {
+            if (currentIndex <= 1) {
+                return points + 1
+            }
+            return points + (currentIndex-1) * 2
+        }, 0)
+        console.info(commonValues,'->',points)
+        return points
+    }).reduce((points, currentPoint) => points + currentPoint, 0))
