9ba87a8b9f3e39eb05f7f4dd9e4f69c1bafe79e4
Hervé Bourzeix <herve.bourzeix@teads.tv>
2023-12-11 00:05:20 +0100

    day 4 part 1

--- day04/main.ts
+++ day04/main.ts
@@ -2,7 +2,7 @@
 
 import * as fs from 'fs'
 
-const input = fs.readFileSync('./input.sample.txt', 'utf-8')
+const input = fs.readFileSync('./input.txt', 'utf-8')
 
 console.info(input.split('\n').filter(line => line !== "").map((line) => {
         const [, cardNumberStr, ownCardsStr, winningCardsStr] = /Card \s*(\d+):((?:\s*\d+\s*)+)\|((?:\s*\d+\s*)+)/.exec(line) || []
@@ -19,12 +19,9 @@ console.info(input.split('\n').filter(line => line !== "").map((line) => {
 ).map(({ownCard, winningCards}) => ownCard.filter(x => winningCards.includes(x)))
     .map((commonValues) => {
 
-        const points =  commonValues.reduce((points, currentValue, currentIndex) => {
-            if (currentIndex <= 1) {
-                return points + 1
-            }
-            return points + (currentIndex-1) * 2
-        }, 0)
+        const points =  commonValues.reduce((points, currentValue, currentIndex) =>
+            2**currentIndex
+        , 0)
         console.info(commonValues,'->',points)
         return points
     }).reduce((points, currentPoint) => points + currentPoint, 0))
